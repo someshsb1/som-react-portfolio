0f008efba7c9c3111630bb3f5325107525ed8e6d
Somesh Balani <someshbalanisb@gmail.com>
2024-01-21 18:08:01 -0500

    projectwork add

--- src/components/ProjectWork.js
+++ src/components/ProjectWork.js
@@ -4,6 +4,7 @@ import project1 from "../assets/2.jpg"
 import project2 from "../assets/3.jpg"
 import project3 from "../assets/4.jpg"
 import project4 from "../assets/5.jpg"
+import project5 from "../assets/9.jpg"
 
 
 const ProjectDataCards = [
@@ -34,6 +35,13 @@ const ProjectDataCards = [
         text: "The design and development of Wi-Fi connectivity based on home security surveillance camera using Raspberry Pi and an IoT Module reducing continuous human monitoring and controlling with enhanced performance and effective results.",
         view: "http://ijarcs.info/index.php/Ijarcs/article/view/5585",
         source: "http://ijarcs.info/index.php/Ijarcs/article/view/5585"
+    }
+    {
+        imgsrc: project5,
+        title: "My Portfolio - ReactJS",
+        text: "Explore my journey, skills, and projects in this dynamic portfolio built with ReactJS. This interactive web application not only showcases my work but also provides a seamless and engaging user experience..",
+        view: "https://someshbalani-portfolio.netlify.app/",
+        source: "https://github.com/someshsb1/som-react-portfolio"
     }
 ];
 
